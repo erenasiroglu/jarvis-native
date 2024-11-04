d3f5e1322153cbdc868ebc34f8cec5e256c7bec1
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-11-04 09:09:48 +0300

    feat: install dependecies

---
+++ App.js
@@ -0,0 +1,10 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-red-200">
+      <Text className="text-[19px]">App</Text>
+    </SafeAreaView>
+  );
+}
